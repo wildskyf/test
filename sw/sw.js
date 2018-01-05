4315fe779a9aefc0c60553424970c0f15e5228d0
wildskyf <poppin.wildsky+gh@gmail.com>
2018-01-05 21:16:42 +0800

    test fetch

--- sw/sw.js
+++ sw/sw.js
@@ -5,7 +5,9 @@ self.addEventListener('install', evt => {
         caches.open('ws-test-cache-v1').then( cache => {
             console.log('open cache and all!');
             setInterval( () => {
-              console.log('yo');
+              fetch('https://api.womany.net/daily_quotes/').then( res => res.json() ).then( data => {
+                console.log(data);
+              })
             }, 2000);
             return cache.addAll(cacheFiles);
         })
