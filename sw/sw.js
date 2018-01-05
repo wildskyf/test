c69e03d0d43888f5fa67605e8e8b28552d55b08c
wildskyf <poppin.wildsky+gh@gmail.com>
2018-01-05 21:41:53 +0800

    cache big pic

--- sw/sw.js
+++ sw/sw.js
@@ -1,11 +1,36 @@
 var cacheFiles = [
   "index.html",
-  "sw.js"
+  "sw.js",
+  "big.jpg"
 ];
+
 self.addEventListener('install', evt => {
     evt.waitUntil(
         caches.open('ws-test-cache-v1').then( cache => {
             return cache.addAll(cacheFiles);
         })
     );
+});
+
+self.addEventListener('fetch', function (evt) {
+    evt.respondWith(
+      caches.match(evt.request).then(function(response) {
+        console.log('fetching', evt.request)
+        if (response) {
+          return response;
+        }
+        var request = evt.request.clone();
+        return fetch(request).then(function (response) {
+          console.log('fetch', response.headers.get('Content-type'));
+          if (!response && response.status !== 200 && !response.headers.get('Content-type').match(/image/)) {
+            return response;
+          }
+          var responseClone = response.clone();
+          caches.open('ws-test-cache-v1').then(function (cache) {
+            cache.put(evt.request, responseClone);
+          });
+          return response;
+        });
+      })
+    )
 });
