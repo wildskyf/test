83772914c628a6944127f4caf3aabf1169a463c4
wildskyf <poppin.wildsky+gh@gmail.com>
2018-01-05 21:50:20 +0800

    yo

--- sw/sw.js
+++ sw/sw.js
@@ -4,33 +4,38 @@ var cacheFiles = [
   "big.jpg"
 ];
 
+const cache_name = 'ws-test-cache-v1';
+
 self.addEventListener('install', evt => {
-    evt.waitUntil(
-        caches.open('ws-test-cache-v1').then( cache => {
-            return cache.addAll(cacheFiles);
-        })
-    );
+  evt.waitUntil(
+    caches.open(cache_name).then( cache => {
+      return cache.addAll(cacheFiles);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (evt) {
-    evt.respondWith(
-      caches.match(evt.request).then(function(response) {
-        console.log('fetching', evt.request)
-        if (response) {
+self.addEventListener('fetch', evt => {
+  console.log('fetching', evt.request);
+
+  evt.respondWith( caches.match(evt.request).then( response => {
+      if (response) {
+        console.log('match!!');
+        return response;
+      }
+
+      var request = evt.request.clone();
+      return fetch(request).then( response => {
+        console.log('fetch', response.headers.get('Content-type'));
+        if (!response && response.status !== 200 && !response.headers.get('Content-type').match(/image/)) {
           return response;
         }
-        var request = evt.request.clone();
-        return fetch(request).then(function (response) {
-          console.log('fetch', response.headers.get('Content-type'));
-          if (!response && response.status !== 200 && !response.headers.get('Content-type').match(/image/)) {
-            return response;
-          }
-          var responseClone = response.clone();
-          caches.open('ws-test-cache-v1').then(function (cache) {
-            cache.put(evt.request, responseClone);
-          });
-          return response;
+        var responseClone = response.clone();
+        caches.open(cache_name).then( cache => {
+          cache.put(evt.request, responseClone);
         });
-      })
-    )
+        return response;
+      });
+    })
+  )
 });
+
