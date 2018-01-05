5277da63e055d4104da80db6c91d0dca6e92bc16
wildskyf <poppin.wildsky+gh@gmail.com>
2018-01-05 21:31:03 +0800

    try to run offline

--- sw/sw.js
+++ sw/sw.js
@@ -1,14 +1,10 @@
 var cacheFiles = [
+  "index.html",
+  "sw.js"
 ];
 self.addEventListener('install', evt => {
     evt.waitUntil(
         caches.open('ws-test-cache-v1').then( cache => {
-            console.log('open cache and all!');
-            setInterval( () => {
-              fetch('https://api.womany.net/daily_quotes/').then( res => res.json() ).then( data => {
-                console.log(data);
-              })
-            }, 2000);
             return cache.addAll(cacheFiles);
         })
     );
