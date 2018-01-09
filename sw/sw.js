44fa9355f862aed6e49c0969105546c2e7f0fdda
wildskyf <poppin.wildsky+gh@gmail.com>
2018-01-10 01:05:37 +0800

    study service worker

--- sw/sw.js
+++ sw/sw.js
@@ -1,4 +1,5 @@
 var cacheFiles = [
+  "/test/sw/",
   "/test/sw/index.html",
   "/test/sw/sw.js",
   "/test/sw/big.jpg"
@@ -6,22 +7,41 @@ var cacheFiles = [
 
 const cache_name = 'ws-test-cache-v1';
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+self.addEventListener('install', event => {
+  console.log('installing');
+  event.waitUntil(
     caches.open(cache_name).then( cache => {
       return cache.addAll(cacheFiles);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then( res => {
-    console.log(res.clone().text());
-    if (res) {
-      console.log('match');
-      return res;
-    }
-    return fetch(e.request);
-  }));
+self.addEventListener('activate', event => {
+  console.log('activing', event);
+  caches.delete(cacne_name);
 });
 
+self.addEventListener('fetch', event => {
+  console.log('fetching url', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request)
+    .then( res => {
+      if (!res) throw({
+        msg: `No cache match url ${event.request.url}`,
+        request: event.request
+      });
+
+      return res.clone();
+    })
+    .catch( err => {
+      console.error(err.msg);
+
+      return fetch(err.request).then( res => {
+        caches.open(cache_name).then( cache => {
+          cache.put(event.request, res);
+        });
+      });
+    })
+  )
+});
